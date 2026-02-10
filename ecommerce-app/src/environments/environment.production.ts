d75ef0a073fc0c9c0bb85b0d45a13bed2bd0277a
Pedro de Lira Gallegos <pedro.delira@sitec.local>
2026-02-10 14:12:13 -0600

    fix: build angular and render deploy

--- ecommerce-app/src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  BACK_URL: 'https://ecommerce-final-api.onrender.com'
-};
--- ecommerce-app/src/environments/environment.ts
+++ ecommerce-app/src/environments/environment.ts
@@ -1,9 +1,4 @@
-/* export const environment = {
+export const environment = {
   production: false,
   BACK_URL: 'http://localhost:3000/api'
-}; */
-
-export const environment = {
-  production: true,
-  BACK_URL: 'https://ecommerce-final-rgki.onrender.com/api'
 };
