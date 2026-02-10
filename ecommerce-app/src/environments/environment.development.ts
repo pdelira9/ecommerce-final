e46695d1b5aff301f4e053bfe9f9ced541bb0205
Pedro de Lira Gallegos <pedro.delira@sitec.local>
2026-02-10 10:21:58 -0600

    Fix production env BACK_URL

--- ecommerce-app/src/environments/environment.development.ts
+++ ecommerce-app/src/environments/environment.development.ts
@@ -1,5 +1,10 @@
-export const environment = {
+/* export const environment = {
   BACK_URL: 'http://localhost:3000/api'//'https://ecommerce-final-api.onrender.com/api',
   //production: true,
   //apiBase: 'https://ecommerce-final-api.onrender.com/'
+}; */
+
+export const environment = {
+  production: false,
+  BACK_URL: 'http://localhost:3000/api'
 };
---
+++ ecommerce-app/src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  BACK_URL: 'https://ecommerce-final-api.onrender.com/api'
+};
