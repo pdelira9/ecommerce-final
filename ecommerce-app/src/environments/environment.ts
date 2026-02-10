b3582d94075eda048ca2fae87e5148f3a475ad46
Pedro de Lira Gallegos <pedro.delira@sitec.local>
2026-02-10 09:08:22 -0600

    Fix BACK_URL

--- ecommerce-app/src/environments/environment.ts
+++ ecommerce-app/src/environments/environment.ts
@@ -1,4 +1,8 @@
-export const environment = {
+/* export const environment = {
   production: false,
   BACK_URL: 'http://localhost:3000/api'
+}; */
+
+export const environment = {
+  BACK_URL: 'https://ecommerce-final-rgki.onrender.com/api'
 };
