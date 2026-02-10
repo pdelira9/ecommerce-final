2a9edce2f27b9d4097da3658d0f23a869334a3db
Pedro de Lira Gallegos <pedro.delira@sitec.local>
2026-02-10 15:46:29 -0600

    agregar ANGULAR_URL

--- ecommerce-app/src/app/core/services/auth/auth.service.ts
+++ ecommerce-app/src/app/core/services/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+/* import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 import { BehaviorSubject, map, Observable, } from 'rxjs';
@@ -27,3 +27,32 @@ export class AuthService {
   }
 }
 
+ */
+
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+export type decodedToken = {
+  userId: string;
+  displayName: string;
+  role: 'admin' | 'customer' | 'guest';
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private baseUrl = `${environment.BACK_URL}`;
+
+  private readonly httpClient = inject(HttpClient);
+
+  checkEmailExist(email: string): Observable<boolean> {
+    return this.httpClient
+      .get<{ exists: boolean }>(`${this.baseUrl}/auth/check-email`, {
+        params: { email },
+      })
+      .pipe(map((res) => res.exists));
+  }
+}
--- ecommerce-app/src/app/core/services/products/products.service.ts
+++ ecommerce-app/src/app/core/services/products/products.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+/* import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, map, Observable, throwError } from 'rxjs';
 import { Product, ProductResponse } from '../../types/Products';
@@ -20,6 +20,81 @@ export type SearchConfig = {
 })
 export class ProductsService {
   private baseUrl = 'http://localhost:3000/api/products';
+
+  private readonly searchConfigSubject = new BehaviorSubject<SearchConfig>({
+    q: '',
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  readonly searchConfig$ = this.searchConfigSubject.asObservable();
+
+  setSearchConfig(cfg: SearchConfig) {
+    this.searchConfigSubject.next(cfg);
+    localStorage.setItem('searchConfig', JSON.stringify(cfg));
+  }
+
+  constructor(private httpClient: HttpClient) {}
+
+  getProducts(page: number = 1, limit: number = 10) {
+    return this.httpClient
+      .get<ProductResponse>(this.baseUrl, { params: { page, limit } })
+      .pipe(catchError((error) => throwError(() => new Error(error))));
+  }
+
+  getProductByID(id: string): Observable<Product> {
+    return this.httpClient.get<Product>(`${this.baseUrl}/${id}`);
+  }
+
+  searchProducts(searchConfig: SearchConfig): Observable<Product[]> {
+    const f: Filters = { q: (searchConfig.q ?? '').trim() };
+
+    if (Number(searchConfig.minPrice) > 0) f.minPrice = Number(searchConfig.minPrice);
+    if (Number(searchConfig.maxPrice) > 0) f.maxPrice = Number(searchConfig.maxPrice);
+
+    const params = new HttpParams({ fromObject: f as any });
+
+    return this.httpClient.get<ProductResponse>(`${this.baseUrl}/search`, { params }).pipe(
+      map((response) => response.products)
+    );
+  }
+
+  searchProductsPaged(searchConfig: SearchConfig, page: number = 1, limit: number = 10) {
+    const f: any = { q: (searchConfig.q ?? '').trim(), page, limit };
+
+    if (Number(searchConfig.minPrice) > 0) f.minPrice = Number(searchConfig.minPrice);
+    if (Number(searchConfig.maxPrice) > 0) f.maxPrice = Number(searchConfig.maxPrice);
+
+    const params = new HttpParams({ fromObject: f });
+
+    return this.httpClient.get<ProductResponse>(`${this.baseUrl}/search`, { params });
+  }
+}
+ */
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, map, Observable, throwError } from 'rxjs';
+import { Product, ProductResponse } from '../../types/Products';
+import { environment } from '../../../../environments/environment';
+
+export type Filters = {
+  q: string;
+  minPrice?: number;
+  maxPrice?: number;
+};
+
+export type SearchConfig = {
+  q: string;
+  minPrice: number;
+  maxPrice: number;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  private baseUrl = `${environment.BACK_URL}/products`;
 
   private readonly searchConfigSubject = new BehaviorSubject<SearchConfig>({
     q: '',
